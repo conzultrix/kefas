6d86f0187552d25d086b40a385c51b6889da6106
Eseosa Aigbogun <eseosa.aigbogun@conzultrix.com>
2013-08-22 05:55:25 +0100

    Removed height and width from drupal image

--- template.php
+++ template.php
@@ -5,3 +5,10 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Kefas theme.
  */
+
+// Remove Height and Width from Drupal Images
+function kefas_preprocess_image(&$variables) {
+  foreach (array('width', 'height') as $key) {
+    unset($variables[$key]);
+  }
+}
